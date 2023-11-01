92358140fd1a3f641be7ce8c579075b93f84bf48
Ali Ramazanov <aliasifzade@gmail.com>
2023-11-01 14:10:23 +0400

    Add small changes

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -1,9 +1,11 @@
 import OurPricingPlans from "../components/OurPricingPlans/OurPricingPlans.jsx";
+import FAQ from "../components/FAQ/FAQ.jsx";
 
 export const Pricing = () => {
   return (
     <div>
       <OurPricingPlans />
+      <FAQ />
     </div>
   );
 };
