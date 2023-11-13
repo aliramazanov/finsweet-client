e12f30fe06f176d884ac5bb7f80fa163afd9365e
Ali Ramazanov <aliasifzade@gmail.com>
2023-11-13 14:14:11 +0400

    Update source of BenefitsOf

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,7 @@
 import OurDesigns from "../components/OurDesigns/OurDesigns";
 import OurMission from "../components/OurMission/OurMission";
 import OurTeam from "../components/OurTeam/OurTeam";
-import BenefitsOf from "../components/BenefitsOf/BenefitsOf";
-
+import BenefitsOf from "../../src/components/BenefitsOF/BenefitsOf";
 export const About = () => {
   return (
     <div>
