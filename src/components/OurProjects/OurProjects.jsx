f61b77823fc3fae5649230f3015d5ff69927a1e2
Ali Ramazanov <aliasifzade@gmail.com>
2023-11-27 15:30:52 +0400

    refactor: template page ui

--- src/components/OurProjects/OurProjects.jsx
+++ src/components/OurProjects/OurProjects.jsx
@@ -1,52 +1,10 @@
-import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import "./OurProjects.scss";
 import card1 from "../../img/card1.svg";
 import card2 from "../../img/card2.svg";
 import card3 from "../../img/card3.svg";
+import "./OurProjects.scss";
 
 export default function OurProjects() {
-  const [isHoveredCard1, setIsHoveredCard1] = useState(false);
-  const [isHoveredCard2, setIsHoveredCard2] = useState(false);
-  const [isHoveredCard3, setIsHoveredCard3] = useState(false);
-
-  const handleMouseEnterCard1 = () => {
-    setIsHoveredCard1(true);
-  };
-
-  const handleMouseLeaveCard1 = () => {
-    setIsHoveredCard1(false);
-  };
-
-  const handleMouseEnterCard2 = () => {
-    setIsHoveredCard2(true);
-  };
-
-  const handleMouseLeaveCard2 = () => {
-    setIsHoveredCard2(false);
-  };
-
-  const handleMouseEnterCard3 = () => {
-    setIsHoveredCard3(true);
-  };
-
-  const handleMouseLeaveCard3 = () => {
-    setIsHoveredCard3(false);
-  };
-
-  const hoverStyleCard1 = {
-    backgroundColor: isHoveredCard1 ? "rgba(0, 0, 255, 0.3)" : "transparent",
-    zIndex: isHoveredCard1 ? 9 : 1,
-  };
-
-  const hoverStyleCard2 = {
-    backgroundColor: isHoveredCard2 ? "rgba(0, 0, 255, 0.3)" : "transparent",
-  };
-
-  const hoverStyleCard3 = {
-    backgroundColor: isHoveredCard3 ? "rgba(0, 0, 255, 0.3)" : "transparent",
-  };
-
   return (
     <div className="ourprojects">
       <section>
@@ -57,29 +15,14 @@ export default function OurProjects() {
       </section>
       <section>
         <div className="all-cards">
-          <div
-            className="big-card"
-            style={hoverStyleCard1}
-            onMouseEnter={handleMouseEnterCard1}
-            onMouseLeave={handleMouseLeaveCard1}
-          >
+          <div className="big-card">
             <img src={card1} alt="" />
           </div>
           <div className="small-cards">
-            <div
-              className="small-card"
-              style={hoverStyleCard2}
-              onMouseEnter={handleMouseEnterCard2}
-              onMouseLeave={handleMouseLeaveCard2}
-            >
+            <div className="small-card">
               <img src={card2} alt="" />
             </div>
-            <div
-              className="small-card"
-              style={hoverStyleCard3}
-              onMouseEnter={handleMouseEnterCard3}
-              onMouseLeave={handleMouseLeaveCard3}
-            >
+            <div className="small-card">
               <img src={card3} alt="" />
             </div>
           </div>
--- src/pages/Template.jsx
+++ src/pages/Template.jsx
@@ -1,106 +1,113 @@
-import "./Pages.scss";
 import template1 from "../../src/img/template1.svg";
 import waywework from "../../src/img/waywework.svg";
+import Footer from "../../src/components/Footer/Footer";
+import "./Pages.scss";
 
 export default function Template() {
   return (
-    <div className="template-container">
-      <div className="template-hero">
-        <p>Web design and development</p>
-        <h2>Finsweet Design case studies</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse.
-        </p>
-        <div className="template-page-img">
-          <img src={template1} alt="" />
+    <div>
+      <div className="template-container">
+        <div className="template-hero">
+          <p>Web design and development</p>
+          <h2>Finsweet Design case studies</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse.
+          </p>
+          <div className="template-page-img">
+            <img src={template1} alt="" />
+          </div>
         </div>
-      </div>
-      <div className="template-info">
-        <div className="template-client-info">
-          <p>Client</p>
-          <span>Facebook</span>
+        <div className="template-info">
+          <div className="template-client-info">
+            <p>Client</p>
+            <span>Facebook</span>
+          </div>
+          <div className="template-client-service">
+            <p>Service</p>
+            <span>Product Design</span>
+          </div>
+          <div className="template-client-deliverable">
+            <p>Deliverable</p>
+            <span>UI Screens, UX Flow & Prototype</span>
+          </div>
         </div>
-        <div className="template-client-service">
-          <p>Service</p>
-          <span>Product Design</span>
+        <div className="break">
+          <hr />
         </div>
-        <div className="template-client-deliverable">
-          <p>Deliverable</p>
-          <span>UI Screens, UX Flow & Prototype</span>
+        <div className="about-project">
+          <h3>About the project</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <ul>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+            <li>
+              Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            </li>
+            <li>
+              Duis aute irure dolor in reprehenderit in voluptate velit esse
+            </li>
+            <li>
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa
+            </li>
+          </ul>
+          <div className="waywework-img">
+            <img src={waywework} alt="" />
+          </div>
+          <h3>How we do it</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <ul>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+            <li>
+              Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            </li>
+            <li>
+              Duis aute irure dolor in reprehenderit in voluptate velit esse
+            </li>
+            <li>
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa
+            </li>
+          </ul>
         </div>
-      </div>
-      <div className="break">
         <hr />
-      </div>
-      <div className="about-project">
-        <h3>About the project</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-        <ul>
-          <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
-          <li>
-            Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-          </li>
-          <li>
-            Duis aute irure dolor in reprehenderit in voluptate velit esse
-          </li>
-          <li>Excepteur sint occaecat cupidatat non proident, sunt in culpa</li>
-        </ul>
-        <div className="waywework-img">
-          <img src={waywework} alt="" />
+        <div className="template-keywords">
+          <h6>Keywords</h6>
+          <p>Design </p>
+          <p>UI/UX </p>
+          <p>Wireframing </p>
+          <p>Branding </p>
+          <p>Development </p>
+          <p>Webflow </p>
         </div>
-        <h3>How we do it</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-        <ul>
-          <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
-          <li>
-            Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-          </li>
-          <li>
-            Duis aute irure dolor in reprehenderit in voluptate velit esse
-          </li>
-          <li>Excepteur sint occaecat cupidatat non proident, sunt in culpa</li>
-        </ul>
+        <hr />
+        <div className="portfolio-ending">
+          <h2>Let&apos;s build something great together</h2>
+          <p>
+            Nullam vitae purus at tortor mattis dapibus. Morbi purus est,
+            ultricies nec dolor sit amet, scelerisque cursus purus.
+          </p>
+          <button>Contact Us</button>
+        </div>
       </div>
-      <hr />
-      <div className="template-keywords">
-        <h6>Keywords</h6>
-        <p>Design </p>
-        <p>UI/UX </p>
-        <p>Wireframing </p>
-        <p>Branding </p>
-        <p>Development </p>
-        <p>Webflow </p>
-      </div>
-      <hr />
-
-      <div className="portfolio-ending">
-        <h2>Let&apos;s build something great together</h2>
-        <p>
-          Nullam vitae purus at tortor mattis dapibus. Morbi purus est,
-          ultricies nec dolor sit amet, scelerisque cursus purus.
-        </p>
-        <button>Contact Us</button>
-      </div>
+      <Footer />
     </div>
   );
 }
